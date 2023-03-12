e7f0409f2c5561c9d86eb1c82c117fb87c318f4e
kamillachdev <kamillachdev@gmail.com>
2023-03-12 12:10:46 +0100

    working menu

--- app/src/main/java/com/example/aplikacja_szkola/MainActivity.kt
+++ app/src/main/java/com/example/aplikacja_szkola/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.aplikacja_szkola
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
+
 //Things to do:
 //1. activity choosing menu on click(toolbar)
-//2. Informacje activity content
-//3. Oceny activity content,
-//4. lock these two activities when user is not logged in
+//2. lock these two activities when user is not logged in
+
+var islogged = false;
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId", "SetTextI18n")
@@ -29,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
         confirmButton.setOnClickListener {
             val login = findViewById<TextInputEditText>(R.id.input_login).text
             val password = findViewById<TextInputEditText>(R.id.input_haslo).text
@@ -42,6 +48,7 @@ class MainActivity : AppCompatActivity() {
                     findViewById<TextView>(R.id.error_haslo).text = ""
                     val intent = Intent(this, Informacje::class.java)
                     startActivity(intent)
+                    islogged = true;
                 }
                 else
                 {
@@ -54,5 +61,47 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.login ->
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            R.id.informacje ->
+            {
+                if(islogged == true)
+                {
+                    val intent = Intent(this, Informacje::class.java)
+                    startActivity(intent)
+                }
+                else
+                {
+                    Toast.makeText(this, "Najpierw się zaloguj!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            R.id.oceny ->
+            {
+                if(islogged == true)
+                {
+                    val intent = Intent(this, Oceny::class.java)
+                    startActivity(intent)
+                }
+                else
+                {
+                    Toast.makeText(this, "Najpierw się zaloguj!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            R.id.logo -> Toast.makeText(this, "Logo", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/aplikacja_szkola/Oceny.kt
+++ app/src/main/java/com/example/aplikacja_szkola/Oceny.kt
@@ -1,11 +1,56 @@
 package com.example.aplikacja_szkola
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class Oceny : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oceny)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.login ->
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            R.id.informacje ->
+            {
+                if(islogged == true)
+                {
+                    val intent = Intent(this, Informacje::class.java)
+                    startActivity(intent)
+                }
+                else
+                {
+                    Toast.makeText(this, "Najpierw się zaloguj!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            R.id.oceny ->
+            {
+                if(islogged == true)
+                {
+                    val intent = Intent(this, Oceny::class.java)
+                    startActivity(intent)
+                }
+                else
+                {
+                    Toast.makeText(this, "Najpierw się zaloguj!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            R.id.logo -> Toast.makeText(this, "Logo", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
